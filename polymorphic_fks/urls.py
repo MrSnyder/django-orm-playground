c665b87d03a93fb8305ea7e6b4053245d75dee3f
Markus Schneider <markus@beefcafe.de>
2021-08-16 10:56:49 +0200

    polymorphic_fks: Added detail views for resources

--- polymorphic_fks/urls.py
+++ polymorphic_fks/urls.py
@@ -1,11 +1,24 @@
 from django.urls import path
 
 from .views import HomeView, CheckrunsWithGenericFk, CheckrunsWithMultipleFks, \
-    CheckrunsWithFksInChildTables, CheckrunsWithDjangoPolymorphic
+    CheckrunsWithFksInChildTables, CheckrunsWithDjangoPolymorphic, OgcServiceDetailView, LayerDetailView, \
+    FeatureTypeDetailView, DatasetMetadataDetailView, ServiceMetadataDetailView, LayerMetadataDetailView, \
+    FeatureTypeMetadataDetailView
 
 app_name = 'polymorphic_fks'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+
+    # resources
+    path('ogcservice/<pk>', OgcServiceDetailView.as_view(), name='ogcservice-detail'),
+    path('layer/<pk>', LayerDetailView.as_view(), name='layer-detail'),
+    path('featuretype/<pk>', FeatureTypeDetailView.as_view(), name='featuretype-detail'),
+    path('dataset-metadata/<pk>', DatasetMetadataDetailView.as_view(), name='dataset-metadata-detail'),
+    path('service-metadata/<pk>', ServiceMetadataDetailView.as_view(), name='service-metadata-detail'),
+    path('layer-metadata/<pk>', LayerMetadataDetailView.as_view(), name='layer-metadata-detail'),
+    path('featuretype-metadata/<pk>', FeatureTypeMetadataDetailView.as_view(), name='featuretype-metadata-detail'),
+
+    # references
     path('checkruns-with-generic-fk/', CheckrunsWithGenericFk.as_view(), name='checkruns-with-generic-fk'),
     path('checkruns-with-multiple-fks/', CheckrunsWithMultipleFks.as_view(), name='checkruns-with-multiple-fks'),
     path('checkruns-with-fks-in-child-tables/', CheckrunsWithFksInChildTables.as_view(),
--- polymorphic_fks/views.py
+++ polymorphic_fks/views.py
@@ -1,9 +1,10 @@
 # Create your views here.
-from django.views.generic import TemplateView
-from django_tables2 import SingleTableView
+from django.views.generic import TemplateView, DetailView
+from django_tables2 import SingleTableView, RequestConfig
 
 from polymorphic_fks.models import CheckrunWithGenericFk, CheckrunWithMultipleFks, \
-    MultiTableBaseCheckrun, DjangoPolymorphicBaseCheckrun
+    MultiTableBaseCheckrun, DjangoPolymorphicBaseCheckrun, OgcService, Layer, FeatureType, ServiceMetadata, \
+    LayerMetadata, FeatureTypeMetadata, DatasetMetadata
 from polymorphic_fks.tables import \
     CheckrunWithMultipleFksTable, MultiTableBaseCheckrunTable, DjangoPolymorphicBaseCheckrunTable, \
     CheckrunWithGenericFkTable
@@ -13,10 +14,54 @@ class HomeView(TemplateView):
     template_name = 'polymorphic_fks/home.html'
 
 
+class OgcServiceDetailView(DetailView):
+    model = OgcService
+
+
+class LayerDetailView(DetailView):
+    model = Layer
+
+
+class FeatureTypeDetailView(DetailView):
+    model = FeatureType
+
+
+class DatasetMetadataDetailView(DetailView):
+    model = DatasetMetadata
+
+
+class ServiceMetadataDetailView(DetailView):
+    model = ServiceMetadata
+
+
+class LayerMetadataDetailView(DetailView):
+    model = LayerMetadata
+
+
+class FeatureTypeMetadataDetailView(DetailView):
+    model = FeatureTypeMetadata
+
+
 class CheckrunsWithGenericFk(SingleTableView):
     model = CheckrunWithGenericFk
     table_class = CheckrunWithGenericFkTable
     template_name = 'polymorphic_fks/checkruns.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.select_related('resource_type')
+
+    def get_table(self, **kwargs):
+        """
+        Return a table object to use. The table has automatic support for
+        sorting and pagination.
+        """
+        table_class = self.get_table_class()
+        table = table_class(data=self.get_table_data(), **kwargs)
+        table.exclude = tuple(self.request.GET.get('exclude', '').split(','))
+        return RequestConfig(self.request, paginate=self.get_table_pagination(table)).configure(
+            table
+        )
 
 
 class CheckrunsWithMultipleFks(SingleTableView):
