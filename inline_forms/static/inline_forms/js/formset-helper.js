139d1c90403983aa187e4038173725307e9638b5
Markus Schneider <markus@beefcafe.de>
2021-08-24 16:16:23 +0200

    inline_forms: Simplified dynamic form handling

--- inline_forms/static/inline_forms/js/formset-helper.js
+++ inline_forms/static/inline_forms/js/formset-helper.js
@@ -1,12 +1,13 @@
 function addForm(prefix) {
-    const templateForm = document.querySelector(`#id-${prefix}-form-template`);
-    const html = templateForm.innerHTML;
-    const formNum = document.querySelectorAll(`.${prefix}-form`).length;
-    const newHtml = html.replace(RegExp(`${prefix}-\\d+-`, 'g'), `${prefix}-${formNum}-`);
-    const newTemplateForm = templateForm.cloneNode();
-    newTemplateForm.innerHTML = newHtml;
-    templateForm.removeAttribute('id')
-    templateForm.removeAttribute('style')
-    templateForm.after(newTemplateForm);
+    const forms = document.querySelectorAll(`.${prefix}-form`);
+    const formNum = forms.length;
+    // last form was the spare form -> becomes the new form now
+    const form = forms [formNum - 1];
+    const html = form.innerHTML;
+    const spareHtml = html.replace(RegExp(`${prefix}-\\d+-`, 'g'), `${prefix}-${formNum}-`);
+    const spareForm = form.cloneNode();
+    spareForm.innerHTML = spareHtml;
+    form.after(spareForm);
+    form.removeAttribute('style')
     document.querySelector(`#id_${prefix}-TOTAL_FORMS`).value=formNum + 1;
 }
